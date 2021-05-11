ff308bc09cd0ca79a8d29ff2717d439fd2defec4
Mircea Popa <mircea.popa12@yahoo.com>
2021-05-11 20:01:23 +0300

    We aded the Hibernate configuration (Pasul 3)

---
+++ src/main/java/com/sda/mirceapopa/hibernate/utils/SessionManager.java
@@ -0,0 +1,31 @@
+package com.sda.mirceapopa.hibernate.utils;
+
+import com.sda.mirceapopa.hibernate.model.Department;
+import com.sda.mirceapopa.hibernate.model.Employee;
+import com.sda.mirceapopa.hibernate.model.Project;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager{
+
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {
+    }
+    public static void shutDown(){
+        INSTANCE.shutdownSessionManager();
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return INSTANCE.getSessionFactory("Humanresources");
+
+    }
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        configuration.addAnnotatedClass(Department.class);
+        configuration.addAnnotatedClass(Employee.class);
+        configuration.addAnnotatedClass(Project.class);
+    }
+
+   
+}
